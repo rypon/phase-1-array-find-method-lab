230049c30cb435fdfc1f2ac7b1934967d2a04fc0
Ryan O'Neill <ryanponeill00@gmail.com>
2022-01-12 17:05:09 -0500

    commit tree ed7c73e96f2a09c3f95fbce56a7e7352eacbcce2

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function superbowlWin(record) {
+    const found = record.find(val => val.result === 'W');
+    if (found === undefined) {
+        return undefined;
+    }
+    return found.year
+}
+
+superbowlWin(result);
+
+
